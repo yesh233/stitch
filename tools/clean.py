c826a4346c5e15b024917ba7ead57bfcde32a5ac
yesh233 <yesh2333@126.com>
2014-07-09 14:10:36 +0800

    previous code

---
+++ tools/clean.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+jobName = sys.argv[1]
+hostNames = ['Master', 'Slave1']
+hdfsFolder = '/home/mfkiller/stitch/hdfs/'
+
+
+for hostName in hostNames:
+	cleanCommand = ('''ssh %s "rm -rf %s%s"''') % (hostName, hdfsFolder, jobName)
+	os.system(cleanCommand)
+
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rmr " + jobName + "/blocks"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rmr " + jobName + "/labels"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rmr " + jobName + "/tmp"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rmr " + jobName + "/label_output"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rmr " + jobName + "/image_output"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rm " + jobName + "/blend.txt"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rm " + jobName + "/block.txt"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rm " + jobName + "/label.txt"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rm " + jobName + "/plane.txt"
+os.system(hdfsClean)
+hdfsClean = "$HADOOP_HOME/bin/hadoop fs -rm " + jobName + "/final.jpg"
+os.system(hdfsClean)
